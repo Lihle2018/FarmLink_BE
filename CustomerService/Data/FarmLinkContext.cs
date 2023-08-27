69811aca6be3bb3705f37535b18bcd500423a25f
Lihle Mqhayi <2018293130@ufs4life.ac.za>
2023-08-27 16:46:16 +0200

    Customer seed

--- CustomerService/Data/FarmLinkContext.cs
+++ CustomerService/Data/FarmLinkContext.cs
@@ -12,7 +12,7 @@ namespace CustomerService.Data
             var database = client.GetDatabase(configuration.GetValue<string>("CustomersTableName"));
 
             Customers = database.GetCollection<Customer>(configuration.GetValue<string>("CustomersCollectionName"));
-            // FarmLinkContextSeed.SeedData(Orders);
+            FarmLinkContextSeed.SeedData(Customers);
         }
         public IMongoCollection<Customer> Customers { get; }
     }
