c7fb5646a833cf614393adb61b87666e978d5014
Lihle Mqhayi <52961460+Lihle2018@users.noreply.github.com>
2023-10-23 09:53:48 +0200

    rabbit mq

---
+++ src/RabbitMq/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace RabbitMq.Services.Interfaces
+{
+    public interface IMessageService
+    { 
+        bool Enqueue(string message);
+    }
+}
---
+++ src/RabbitMq/Services/MessageService.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMq.Services.Interfaces;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMq.Services
+{
+    public class MessageService : IMessageService
+    {
+        ConnectionFactory _factory;
+        IConnection _conn;
+        IModel _channel;
+
+        public MessageService()
+        {
+            _factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
+            _factory.UserName = "guest";
+            _factory.Password = "guest";
+            _conn = _factory.CreateConnection();
+            _channel = _conn.CreateModel();
+            _channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+        }
+        public bool Enqueue(string messageString)
+        {
+            var body = Encoding.UTF8.GetBytes("server processed " + messageString);
+            _channel.BasicPublish(exchange: "",
+                                routingKey: "hello",
+                                basicProperties: null,
+                                body: body);
+            
+            return true;
+        }
+    }
+}
